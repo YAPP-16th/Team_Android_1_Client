10462487b48f33cd43948ba28b8635cfe7e4b209
junghyun park <parkmint.urssu@gmail.com>
2020-04-05 02:20:59 +0900

    login onboarding view finished

---
+++ data/src/main/java/com/eroom/data/localclass/DesignClass.kt
@@ -0,0 +1,21 @@
+package com.eroom.data.localclass
+
+enum class DesignClass(private val itemName: String) {
+    UX("UX 디자인"),
+    UIGUI("UI/GUI 디자인"),
+    VIDEOMOTION("영상/모션 디자인"),
+    EDIT("편집 디자인"),
+    BX("BX 디자인"),
+    WEB("웹 디자인"),
+    MOBILE("모바일 디자인"),
+    GRAPIC("그래픽 디자인"),
+    PACKAGE("패키지 디자인");
+
+    fun getName() = itemName
+
+    companion object {
+        fun getArray() = arrayListOf(
+            UX, UIGUI, VIDEOMOTION, EDIT, BX, WEB, MOBILE, GRAPIC, PACKAGE
+        )
+    }
+}
