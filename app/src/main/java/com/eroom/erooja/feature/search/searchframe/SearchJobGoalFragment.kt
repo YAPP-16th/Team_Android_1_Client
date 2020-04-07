5b704f1d72c6748dda20ad8e5380a89851a671c1
jsh <ppm_it@naver.com>
2020-04-08 02:05:13 +0900

    Search ViewPagar Fragment Added

---
+++ app/src/main/java/com/eroom/erooja/feature/search/searchframe/SearchJobGoalFragment.kt
@@ -0,0 +1,24 @@
+package com.eroom.erooja.feature.search.searchframe
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.eroom.erooja.R
+
+class SearchJobGoalFragment : Fragment() {
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = SearchJobGoalFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search_job_goal, container, false)
+    }
+
+}
