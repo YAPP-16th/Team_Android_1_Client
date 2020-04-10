48a77b68475747d2580b4af053a83fc095b2d314
dokyun Kim <rangskim@naver.com>
2020-04-11 05:44:29 +0900

    calendar module added & endDate setting added

--- app/src/main/java/com/eroom/erooja/feature/addGoal/newGoalPage/GoalPeriodFragment.kt
+++ app/src/main/java/com/eroom/erooja/feature/addGoal/newGoalPage/GoalPeriodFragment.kt
@@ -7,18 +7,49 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.eroom.erooja.R
+import com.eroom.erooja.databinding.FragmentGoalPeriodBinding
+import com.eroom.erooja.feature.addGoal.newGoalFrame.NewGoalActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class GoalPeriodFragment : Fragment() {
-
+    private lateinit var goalPeriodBinding: FragmentGoalPeriodBinding
+    //val
+    companion object {
+        @JvmStatic
+        fun newInstance() = GoalPeriodFragment()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_goal_period, container, false)
+        setUpDataBinding(inflater, container)
+        initView()
+        return goalPeriodBinding.root
     }
 
+    private fun setUpDataBinding(inflater: LayoutInflater, container: ViewGroup?) {
+        goalPeriodBinding = FragmentGoalPeriodBinding.inflate(inflater, container, false)
+        goalPeriodBinding.fragment = this
+    }
+
+    private fun initView() {
+        setStartDate()
+    }
+
+    private fun setStartDate() {
+        val currentTime: Date = Calendar.getInstance().time
+        goalPeriodBinding.startDateContent.text = SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault()).format(currentTime)
+    }
+
+    fun onCalendarClicked() {
+        (activity as NewGoalActivity).calendarCall()
+    }
+
+    fun setEndDate(endDate:String) {
+        goalPeriodBinding.endDateContent.text = endDate
+
+    }
 }
