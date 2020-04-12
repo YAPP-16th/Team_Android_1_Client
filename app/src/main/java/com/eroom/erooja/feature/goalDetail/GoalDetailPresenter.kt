07b4e9a7e99bdb4432c87f0b9ffb298a41f3a091
jsh <ppm_it@naver.com>
2020-04-12 15:36:10 +0900

    Revert commit
    This reverts commit 5d37a6573d16eb70f87f2cdbb850d7514a33a287.

--- app/src/main/java/com/eroom/erooja/feature/goalDetail/GoalDetailPresenter.kt
+++ app/src/main/java/com/eroom/erooja/feature/goalDetail/GoalDetailPresenter.kt
@@ -7,8 +7,6 @@ class GoalDetailPresenter(override var view: GoalDetailContract.View)
     :GoalDetailContract.Presenter{
 
     var tempdata: ArrayList<UserSimpleData> = arrayListOf(UserSimpleData("sehee", 40, 20,"ㄸㄸㄸ","ㅁㄴㅇㄹ","ㄴㅇㄹㄴㅇ"),
-        UserSimpleData("somebody", 99, 22, "im shefm", "asdfe", " asdfeasg"),
-        UserSimpleData("somebody", 99, 22, "im shefm", "asdfe", " asdfeasg"),
         UserSimpleData("somebody", 99, 22, "im shefm", "asdfe", " asdfeasg"))
 
 
