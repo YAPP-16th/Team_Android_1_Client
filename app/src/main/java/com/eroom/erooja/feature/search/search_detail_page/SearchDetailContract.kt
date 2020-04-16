4c4826900b8d9be49d64e124944eae03eb311567
jsh <ppm_it@naver.com>
2020-04-17 02:15:48 +0900

    리뷰반영

--- app/src/main/java/com/eroom/erooja/feature/search/search_detail_page/SearchDetailContract.kt
+++ app/src/main/java/com/eroom/erooja/feature/search/search_detail_page/SearchDetailContract.kt
@@ -2,7 +2,7 @@ package com.eroom.erooja.feature.search.search_detail_page
 
 interface SearchDetailContract {
     interface View {
-        fun changeView(pos : Int)
+        fun changeView(pos: Int)
     }
 
     interface Presenter {
