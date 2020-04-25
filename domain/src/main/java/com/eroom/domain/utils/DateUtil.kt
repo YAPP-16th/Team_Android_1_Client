02c8a044d83b3bb7af84c2d42b869b2ba6f33972
ddochi9 <rangskim@naver.com>
2020-04-25 15:55:14 +0900

    DateUtil changed to 23:59:59

--- domain/src/main/java/com/eroom/domain/utils/DateUtil.kt
+++ domain/src/main/java/com/eroom/domain/utils/DateUtil.kt
@@ -3,9 +3,9 @@ package com.eroom.domain.utils
 fun toLocalDateFormat(year: Int, month: Int, day: Int): String {
     val tMonth = if (month < 10) "0$month" else "$month"
     val tDay = if (day < 10) "0$day" else "$day"
-    return "$year-$tMonth-${tDay}T00:00:00"
+    return "$year-$tMonth-${tDay}T23:59:59"
 }
 
 fun toLocalDateFormat(year: String, month: String, day: String): String {
-    return "$year-$month-${day}T00:00:00"
+    return "$year-$month-${day}T23:59:59"
 }
