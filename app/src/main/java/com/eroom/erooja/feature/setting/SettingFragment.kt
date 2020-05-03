32dffb444a1e9b3c9626472f021c97f2ed6d1c67
jsh <ppm_it@naver.com>
2020-05-04 06:05:42 +0900

    setting detail (notification, help, opensource, profileimg) view added

--- app/src/main/java/com/eroom/erooja/feature/setting/SettingFragment.kt
+++ app/src/main/java/com/eroom/erooja/feature/setting/SettingFragment.kt
@@ -1,20 +1,26 @@
 package com.eroom.erooja.feature.setting
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.eroom.domain.globalconst.Consts
 import com.eroom.erooja.R
 import com.eroom.erooja.databinding.FragmentSettingBinding
+import com.eroom.erooja.feature.goalDetail.GoalDetailActivity
+import com.eroom.erooja.feature.setting.setting_detail.*
 
 class SettingFragment :Fragment(), SettingContract.View{
     private lateinit var settingBinding: FragmentSettingBinding
     private lateinit var presenter : SettingPresenter
 
     private lateinit var settingList : Array<String>
+    private lateinit var activity : ArrayList<Activity>
 
     companion object {
         @JvmStatic
@@ -28,6 +34,7 @@ class SettingFragment :Fragment(), SettingContract.View{
     ): View? {
         setUpDataBinding(inflater, container)
         initView()
+        initActivity()
         return settingBinding.root
     }
 
@@ -43,6 +50,19 @@ class SettingFragment :Fragment(), SettingContract.View{
 
     }
 
+    private fun initActivity(){
+        activity = ArrayList()
+        activity.apply{
+            addAll(listOf(
+                AlarmActivity(),
+                ProfileActivity(),
+                JobInterestActivity(),
+                HelpActivity(),
+                OpensourceActivity()
+            ))
+        }
+    }
+
     override fun setList(list: Array<String>) {
         settingBinding.settingRecycler.apply{
             layoutManager = LinearLayoutManager(context)
@@ -51,8 +71,6 @@ class SettingFragment :Fragment(), SettingContract.View{
     }
 
     private var click = { position: Int ->
-        when(position){
-            0-> {}
-        }
+        startActivity(Intent(context, activity[position]::class.java))
     }
 }
---
+++ app/src/main/java/com/eroom/erooja/feature/setting/setting_detail/JobInterestActivity.kt
@@ -0,0 +1,11 @@
+package com.eroom.erooja.feature.setting.setting_detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.eroom.erooja.R
+
+class JobInterestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
