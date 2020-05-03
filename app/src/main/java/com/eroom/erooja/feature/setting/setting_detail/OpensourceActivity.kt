8261c0a41bcc1aa8bae12dd822678eab14e2ba45
jsh <ppm_it@naver.com>
2020-05-04 06:05:42 +0900

    setting detail (notification, help, opensource, profileimg) view added

---
+++ app/src/main/java/com/eroom/erooja/feature/setting/setting_detail/OpensourceActivity.kt
@@ -0,0 +1,13 @@
+package com.eroom.erooja.feature.setting.setting_detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.eroom.erooja.R
+
+class OpensourceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_licence_setting)
+    }
+}
---
+++ app/src/main/java/com/eroom/erooja/feature/setting/setting_detail/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.eroom.erooja.feature.setting.setting_detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.eroom.erooja.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_change_profile_setting)
+    }
+}
