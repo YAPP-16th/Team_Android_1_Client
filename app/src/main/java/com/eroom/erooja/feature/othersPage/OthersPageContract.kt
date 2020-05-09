a075ca6f0c9ab8c707e7d3804c4da10e866ad8c3
ddochi9 <rangskim@naver.com>
2020-05-09 20:59:25 +0900

    othersPage Activity added

---
+++ app/src/main/java/com/eroom/erooja/feature/othersPage/OthersPageContract.kt
@@ -0,0 +1,27 @@
+package com.eroom.erooja.feature.othersPage
+
+import com.eroom.data.entity.JobClass
+import com.eroom.data.entity.MinimalGoalDetailContent
+import com.eroom.erooja.feature.mypage.MyPageContract
+
+interface OthersPageContract {
+    interface View {
+        fun setNickname(nickname: String)
+        fun setJobInterestInfo(classList: ArrayList<JobClass>)
+        fun saveUid(uid: String)
+        fun setOngoingGoalList(list: ArrayList<MinimalGoalDetailContent>)
+        fun setOnGoingGoalPageIsEnd(isEnd: Boolean)
+        fun setEndedGoalPageIsEnd(isEnd: Boolean)
+        fun setEndedGoalList(list: ArrayList<MinimalGoalDetailContent>)
+        fun setOngoingGoalListSizeOnTabLayout(totalElements: Int)
+        fun setEndedGoalListSizeOnTabLayout(totalElements: Int)
+    }
+
+    interface Presenter {
+        val view: View
+        fun getMemberJobInterest()
+        fun onCleared()
+        fun getOngoingGoalList(uid: String, page: Int)
+        fun getEndedGoalList(uid: String, page: Int)
+    }
+}
