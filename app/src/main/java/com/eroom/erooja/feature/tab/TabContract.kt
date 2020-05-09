1724c5865f7e4cfdc10c674894be4fcf0723703d
junghyun park <parkmint.urssu@gmail.com>
2020-05-10 07:44:00 +0900

    fragment error fix

--- app/src/main/java/com/eroom/erooja/feature/tab/TabContract.kt
+++ app/src/main/java/com/eroom/erooja/feature/tab/TabContract.kt
@@ -1,13 +1,14 @@
 package com.eroom.erooja.feature.tab
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.eroom.data.entity.JobGroup
 import com.eroom.data.response.JobGroupAndClassResponse
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 interface TabContract {
     interface View{
-        fun loadFragment(index: Int): Fragment
+        fun loadFragment(index: Int): Int
     }
 
     interface Presenter {
