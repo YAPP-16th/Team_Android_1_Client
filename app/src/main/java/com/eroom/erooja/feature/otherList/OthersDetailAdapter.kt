1557ac229bdeb6f16cd67741b86b6de956405753
jsh <ppm_it@naver.com>
2020-05-21 00:40:56 +0900

    todo check list 동기화작업

--- app/src/main/java/com/eroom/erooja/feature/otherList/OthersDetailAdapter.kt
+++ app/src/main/java/com/eroom/erooja/feature/otherList/OthersDetailAdapter.kt
@@ -1,5 +1,6 @@
 package com.eroom.erooja.feature.otherList
 
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,13 +8,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.eroom.data.entity.MinimalTodoListDetail
 import com.eroom.erooja.R
 import kotlinx.android.synthetic.main.item_completed_goal_list.view.*
+import kotlinx.android.synthetic.main.item_ongoing_goal_list.view.*
 
-class OthersDetailAdapter(private val TodoList: ArrayList<MinimalTodoListDetail>):
+class OthersDetailAdapter(private val TodoList: ArrayList<MinimalTodoListDetail>) :
     RecyclerView.Adapter<Holder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val inflatedView =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_completed_goal_list, parent, false)
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_completed_goal_list, parent, false)
         return Holder(inflatedView)
 
     }
@@ -21,12 +24,22 @@ class OthersDetailAdapter(private val TodoList: ArrayList<MinimalTodoListDetail>
     override fun getItemCount(): Int = TodoList.size
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bind(TodoList[position].content)
+        holder.bind(TodoList[position].content, TodoList[position].isEnd)
     }
 }
 
-class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
-    fun bind(text:String){
-        itemView.completed_detail_checkbox.text=text
+class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(contentText: String, isEnd: Boolean) {
+        itemView.completed_detail_checkbox.text = contentText
+        itemView.completed_detail_checkbox.apply {
+            isChecked = isEnd
+            if (isEnd) {
+                paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                setTextColor(context.getColor(R.color.grey4))
+            } else {
+                paintFlags = 0
+                setTextColor(context.getColor(R.color.grey7))
+            }
+        }
     }
 }
